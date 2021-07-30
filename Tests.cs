11f91d8ca392105e99e45267ad4dcbd070f91a93
exqzmepls <hm>
2021-07-30 21:05:44 +0500

    Определен OneTimeSetUp

--- Tests.cs
+++ Tests.cs
@@ -1,8 +1,17 @@
 using NUnit.Framework;
+using System.Net.Http;
 
 namespace TestTask
 {
+    [TestFixture]
     public class Tests
     {
+        private HttpClient _client;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _client = new HttpClient();
+        }
     }
 }
