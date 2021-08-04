365e7983a1a75d92fd0f1a730e9da11c0ce95f3f
exqzmepls <hm>
2021-08-04 13:54:07 +0500

    Добавлен класс для события

---
+++ Event.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    public class Event
+    {
+        public string EventId { get; set; }
+
+        public string Timestamp { get; set; }
+
+        public string EventDescription { get; set; }
+
+        public bool IsAlarmEvent { get; set; }
+
+        public string ChannelId { get; set; }
+
+        public string ChannelName { get; set; }
+
+        public string Zoneid { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
